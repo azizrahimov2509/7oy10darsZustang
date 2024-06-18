eb040fe3926b7efc2242427159de3f239cee2ee1
azizrahimov2509 <rahimov2509@mail.com>
2024-06-19 00:49:35 +0300

    ZustangMarket

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,39 @@
 import { create } from "zustand";
 
 export const useStore = create((set) => ({
-  count: 0,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-  decrement: () => set((state) => ({ count: state.count - 1 })),
+  //cart
+  cart: JSON.parse(localStorage.getItem("cart")) ?? [],
+  addToCart: (id) =>
+    set((state) => {
+      const index = state.cart.findIndex((item) => item.id === id);
+      if (index !== -1) {
+        return {
+          ...state,
+          cart: state.cart.map((item, idx) =>
+            idx === index ? { ...item, count: item.count + 1 } : item
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cart: [...state.cart, { id, count: 1 }],
+        };
+      }
+    }),
+  incrementCount: (id) =>
+    set((state) => ({
+      ...state,
+      cart: state.cart.map((item) =>
+        item.id === id ? { ...item, count: item.count + 1 } : item
+      ),
+    })),
+  decrementCount: (id) =>
+    set((state) => ({
+      ...state,
+      cart: state.cart
+        .map((item) =>
+          item.id === id ? { ...item, count: item.count - 1 } : item
+        )
+        .filter((item) => item.count > 0),
+    })),
 }));
